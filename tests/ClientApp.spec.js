60e1cd9e19fb6dd3160758e6afa45ccc17444813
unknown <pavel-bq>
2023-08-04 12:11:12 +0200

    Abschnitt 4

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,5 +1,19 @@
 const {test, expect} = require('@playwright/test');
 
+test('Browser Context-Validation Error login', async ({page})=>
+{
+   await page.goto("https://rahulshettyacademy.com/client")
+
+   await page.locator("#userEmail").fill("paveldon@web.de")
+   await page.locator("#userPassword").type("paveL#123")
+   await page.locator("[value='Login']").click();
+
+   await page.waitForLoadState('networkidle');
+   const titles = await page.locator(".card-body b").allTextContents();
+   
+   console.log(titles)
+});
+
 test.only('Browser Context-Validation Error login', async ({page})=>
 {
    await page.goto("https://rahulshettyacademy.com/client")
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,35 +1,73 @@
 const {test, expect} = require('@playwright/test');
 
-test.only('Browser Playwritght Test', async ({browser})=>
+test('Browser Playwritght Test', async ({browser})=>
 {
     // plugins/coolis
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
-
     const userName = page.locator('#username');
     const userPass = page.locator("[type='password']");
     const singIn = page.locator('#signInBtn');
-
+    const cartdTitles = await page.locator(".card-body a").allTextContents();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     await userName.type('rahulshetty');
     await userPass.type('learning');
     await singIn.click();
     console.log(await page.locator("[style*='block']").textContent());
-    await expect(page.locator("[style*='block']")).toContainText('Incorrect')
-
-    await userName.fill('');
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect');
     await userName.fill('rahulshettyacademy');
-
+    await userName.fill('learning');
     await Promise.all([
         page.waitForURL(),
         singIn.click()
     ]);
 
+    console.log(await cartdTitles.first().textContent());
+    console.log(await cartdTitles.nth(1).textContent());
+    const allTitles = await cartdTitles.allTextContents();
+    console.log(allTitles);
+
 });
 
-test('Page Playwritght Test', async ({page})=>
+test('Web UI Controls', async ({page})=>
 {
-    await page.goto("https://google.com")
-    await expect(page).toHaveTitle("Google")
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const userName = page.locator('#username');
+    const signIn = page.locator("#signInBtn");
+    const documentLink = page.locator("[href*='documents-request']");
+    const dropdown = page.locator("select.form-control");
+    await dropdown.selectOption("consult");
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click();
+    console.log(await page.locator(".radiotextsty").last().isChecked());
+    await expect(page.locator(".radiotextsty").last()).toBeChecked();  
+    await page.locator("#terms").click();
+    await expect( page.locator("#terms")).toBeChecked();
+    await page.locator("#terms").uncheck();
+    expect( await page.locator("#terms").isChecked()).toBeFalsy();
+    await expect(documentLink).toHaveAttribute("class","blinkingText")
 
 });
+
+test.only('Child windows hadl', async ({browser})=>
+{
+    const context = await browser.newContext();
+    const page =  await context.newPage();
+    const userName = page.locator('#username');
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator("[href*='documents-request']");
+
+    const [newPage] = await  Promise.all([
+        context.waitForEvent('page'),
+        documentLink.click()
+    ])
+   
+    const  text = await newPage.locator(".red").textContent();
+    console.log(text);
+    const arrayText = text.split("@");
+    console.log(arrayText);
+    const domain =  arrayText[1].split(" ")[0];
+    console.log(domain);
+    await page.locator("#username").type(domain);
+    console.log(await page.locator("#username").textContent());
+})
